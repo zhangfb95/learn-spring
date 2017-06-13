9ab77f8a9e09ce3e7b21d8a3d2a30a1334530419
zhangfb <zhangfb@shishike.com>
2017-06-13 15:51:24 +0800

    add three number's x testcase

--- guess/src/test/java/com/learn/spring/guess/GameTest.java
+++ guess/src/test/java/com/learn/spring/guess/GameTest.java
@@ -54,4 +54,17 @@ public class GameTest {
         String output = game.call(firstNumber * secondNumber);
         assertThat(output, is(OutputEnum.FIZZ.getName() + OutputEnum.BUZZ.getName()));
     }
+
+    @Test
+    public void should_say_fizzbusswhizz_when_input_is_multiple_of_first_x_second_x_third() {
+        final int firstNumber = 3;
+        final int secondNumber = 5;
+        final int thirdNumber = 7;
+        int[] numbers = {firstNumber, secondNumber, thirdNumber};
+        SpecialNumbers specialNumbers = new SpecialNumbers(numbers);
+        Game game = new Game(specialNumbers);
+
+        String output = game.call(firstNumber * secondNumber * thirdNumber);
+        assertThat(output, is(OutputEnum.FIZZ.getName() + OutputEnum.BUZZ.getName() + OutputEnum.WHIZZ.getName()));
+    }
 }
