10a9439b47025b90279897386b5f080dc9e9cc3b
zhangfb <zhangfb@shishike.com>
2018-12-12 17:09:53 +0800

    代码重构

---
+++ ioc/src/main/java/com/juconcurrent/learn/spring/ioc/Main.java
@@ -0,0 +1,17 @@
+package com.juconcurrent.learn.spring.ioc;
+
+import com.juconcurrent.learn.spring.ioc.vo.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author zhangfb
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        User user = applicationContext.getBean("user", User.class);
+        System.out.println(user);
+    }
+}
---
+++ ioc/src/main/java/com/juconcurrent/learn/spring/ioc/vo/User.java
@@ -0,0 +1,12 @@
+package com.juconcurrent.learn.spring.ioc.vo;
+
+import lombok.Data;
+
+/**
+ * @author zhangfb
+ */
+@Data public class User {
+
+    private String username;
+    private String password;
+}
