799368df1d6ede0a71b076a09cd9a13db8ad6467
zhangfb95 <zhangfb95@gmail.com>
2019-09-08 11:56:07 +0800

    add it

--- ioc/src/main/java/com/juconcurrent/learn/spring/ioc/KnightMain.java
+++ ioc/src/main/java/com/juconcurrent/learn/spring/ioc/KnightMain.java
@@ -1,12 +1,14 @@
 package com.juconcurrent.learn.spring.ioc;
 
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class KnightMain {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("META-INF/spring/knights.xml");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knights.xml");
+        // ConfigurableApplicationContext context = new FileSystemXmlApplicationContext("file path");
+        //ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(KnightConfig.class);
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
